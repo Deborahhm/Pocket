bef2b8e5a336283510adfe845e4ed8d5d00e20e1
Deborah Nascimento <deborahmdn@bct.ect.ufrn.br>
2023-01-30 20:54:42 +0000

    [style]: changing button color

--- app.js
+++ app.js
@@ -14,50 +14,72 @@ onEvent("reset", "click", () => {
     carteira = document.querySelector("#dinheiro").value;
     economia = document.querySelector("#economias").value;
     extrato = ""; 
-    
-})
-
-onEvent("reset", "click", () => {
+    document.querySelector("#food").value = " ";  
+    document.querySelector("#fun").value = " ";  
+    document.querySelector("#health").value = " ";  
     document.querySelector("#wallet").innerHTML = carteira;
     document.querySelector("#dinheiro").value = " ";
     document.querySelector("#economias").value = " ";
+    document.querySelector("#wallet-button").style.backgroundColor = "#111827";     
 })
+
+
 
 
 
 onEvent("comida","click", () =>{
     let comida = document.querySelector("#food").value;
     extrato += "Saldo Atual: " + carteira + " reais. \n"; 
-    carteira -= comida; 
-    extrato += comida + " reais debitados na sua conta \n"; 
-    document.querySelector("#wallet").innerHTML = carteira;
-    document.querySelector("#food").value = " ";    
+    let condicao = carteira - comida;
+    if(condicao > 0){
+        carteira -= comida; 
+        extrato += comida + " reais debitados na sua conta \n"; 
+        document.querySelector("#wallet").innerHTML = carteira;
+        document.querySelector("#food").value = " ";    
+        if(carteira <= economia){
+            document.querySelector("#wallet-button").style.backgroundColor = "#FA3E4C";
+            alert("Você Gastou mais do que queria economizar");
+        }
+    }else{
+        alert("Você não tem mais dinheiro"); 
+    }
 });
 
 
 onEvent("brinquedo","click", () => {
     let brinquedo = document.querySelector("#fun").value;
     extrato += "Saldo Atual: " + carteira + " reais. \n"; 
-    carteira -= brinquedo;
-    extrato += brinquedo + " reais debitados na sua conta \n"; 
-    document.querySelector("#fun").value = " ";
-    document.querySelector("#wallet").innerHTML = carteira;
-    if(carteira < 0){
-        document.querySelector("#wallet-button").style.backgroundCollor = "#FA3E4C";
+    let condicao = carteira - brinquedo;
+   if(condicao > 0){
+        carteira -= brinquedo;
+        extrato += brinquedo + " reais debitados na sua conta \n"; 
+        document.querySelector("#fun").value = " ";
+        document.querySelector("#wallet").innerHTML = carteira;
+        if(carteira <= economia){
+            alert("Você Gastou mais do que queria economizar");
+            document.querySelector("#wallet-button").style.backgroundColor = "#FA3E4C";
+        }
+   }else{
+        alert("Você não tem mais dinheiro"); 
     }
-
 })
 
 
 onEvent("saude","click", () => {
     let saude =  document.querySelector("#health").value;
     extrato += "Saldo Atual: " + carteira + " reais. \n";
+    let condicao = carteira - saude;
+    if(condicao > 0){
     carteira -= saude; 
     extrato += brinquedo + " reais debitados na sua conta \n"; 
     document.querySelector("#wallet").innerHTML = carteira;
     document.querySelector("#health").value = " ";
-    if(carteira < 0){
-        document.querySelector("#wallet-button").style.backgroundCollor = "#FA3E4C";
+    if(carteira <= economia){
+        document.querySelector("#wallet-button").style.backgroundColor = "#FA3E4C";
+        alert("Você Gastou mais do que queria economizar");
+    }
+    }else{
+        alert("Você não tem mais dinheiro");         
     }
 })
 
