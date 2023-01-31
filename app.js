3e2c3fd5ef5941e59016f11813d7ddf07d1c22f5
Deborah Nascimento <deborahmdn@bct.ect.ufrn.br>
2023-01-31 23:33:42 +0000

    [update]: list to extract

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ import {
 } from './lib/code.org.js';
 
 let carteira = 0; 
-let extrato = " "; 
+let extrato = []; 
 const comida = 0; 
 const saude = 0; 
 const brinquedo = 0; 
 let economia = 0;
 
-
+//carrega o valor digitado na carteira e limpa os campos de input de economia e carteira
 onEvent("reset", "click", () => {
     carteira = document.querySelector("#dinheiro").value;
     economia = document.querySelector("#economias").value;
@@ -25,18 +25,22 @@ onEvent("reset", "click", () => {
 })
 
 
-
+//Quando o botao comida é clicado exucuta 
 onEvent("comida","click", () =>{
+    //pega o valor do input para reduzir da carteira
     let comida = document.querySelector("#food").value;
-    carteira = alertaUsuario(comida, carteira, economia);
+    //chama a funcao para atualizar a carteira (ver funcao)
+    carteira = AtualizaCarteira(comida, carteira, economia);
+    //atualiza o valor da carteira 
     document.querySelector("#wallet").innerHTML = carteira;
+    //limpa o valor digitado no input (ver funcao)
     cleanValue();  
 });
 
 
 onEvent("brinquedo","click", () => {
     let brinquedo = document.querySelector("#fun").value; 
-    carteira = alertaUsuario(brinquedo, carteira, economia);
+    carteira = AtualizaCarteira(brinquedo, carteira, economia);
     document.querySelector("#wallet").innerHTML = carteira;
     cleanValue();  
 })
@@ -44,38 +48,44 @@ onEvent("brinquedo","click", () => {
 
 onEvent("saude","click", () => {
     let saude =  document.querySelector("#health").value;
-    carteira = alertaUsuario(saude, carteira, economia);
+    carteira = AtualizaCarteira(saude, carteira, economia);
     document.querySelector("#wallet").innerHTML = carteira;
     cleanValue(); 
 })
 
+
+//imprime o extrato quando o botao é clicado
 onEvent("go-extrato", "click", () => {
     alert(extrato);
 })
 
 
-
+//limpa o valor digitado no input
 const cleanValue = () => {
     document.querySelector("#food").value = " ";
     document.querySelector("#fun").value = " ";
     document.querySelector("#health").value = " ";
 }
 
-//terminar essa parte aqui 
 
-const alertaUsuario = (id, carteira, economia) => {
-    extrato += "Saldo Atual: " + carteira + " reais. \n";
+
+const AtualizaCarteira = (id, carteira, economia) => {
+    extrato.push("Saldo Atual: " + carteira + " reais. \n");
+    //Se o valor final for maior que zero, tire o valor da carteira
     if((carteira - id >= 0) && (id != NaN)) {
         carteira -= id;
-        extrato += id + " reais debitados na sua conta \n";
+        extrato.push(id + " reais debitados na sua conta \n");
+        //se o valor for menor que o valor que queremos economizar, deixe o botao vermelho
         if(carteira <= economia){
             document.querySelector("#wallet-button").style.backgroundColor = "#FA3E4C";
             alert("Você Gastou mais do que queria economizar");
             
         }
+        //Se o valor for menor que zero nao debite nada, pq obviamente voce nao tem dinheiro.
     }else if((condicao < 0) &&(id != NaN)){
         alert("Você não tem mais dinheiro");
     }else if(id === NaN){
+        //checa para saber se o usuario nao digitou besteira
         alert("Isso não é um número!");
     }
     return carteira;
